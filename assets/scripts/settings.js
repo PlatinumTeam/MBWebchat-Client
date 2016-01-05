9d85f78a6537760092289695b1cb6e837d231512
HiGuy Smith <higuymb@gmail.com>
2016-01-04 22:09:28 -0500

    Better saving in localstorage

--- assets/scripts/settings.js
+++ assets/scripts/settings.js
@@ -1,49 +1,51 @@
 (function($){
 	function Settings() {
 		this.notifications = {
-			login: false,
-			logout: false,
-			setlocation: false,
-			levelup: false,
-			mastery: false,
-			taskcomplete: false,
-			achievement: false,
-			prestigeup: false,
-			record: false
+			login: true,
+			levelup: true,
+			mastery: true,
+			taskcomplete: true,
+			achievement: true,
+			prestigeup: true,
+			record: true
 		};
 	}
 	Settings.prototype.loadLocal = function() {
+		//Get all of localStorage's fields for ourselves
 		if (typeof(localStorage.settings) !== "undefined") {
-			Object.keys(localStorage.settings).forEach(function(key) {
-				settings[key] = localStorage.settings[key];
-			});
+			try {
+				var localSettings = JSON.parse(localStorage.settings);
+				Object.keys(localSettings).forEach(function(key) {
+					settings.notifications[key] = localSettings[key];
+				});
+			} catch (e) {
+
+			}
 		}
-	}
+	};
 	Settings.prototype.saveLocal = function() {
-		var temp = {};
-		Object.keys(this).forEach(function(key) {
-			if (typeof(settings[key]) !== "function")
-				temp[key] = settings[key];
-		});
-		localStorage.settings = temp;
-	}
+		//Put all of our fields into localStorage
+		localStorage.settings = JSON.stringify(this.notifications);
+	};
 	Settings.prototype.shouldShowNotification = function(type) {
+		//Show notifications if we don't know what they are, because we haven't explicitly disabled them
 		if (typeof(this.notifications[type]) === "undefined")
 			return true;
 		return this.notifications[type];
-	}
+	};
 	Settings.prototype.open = function() {
-		$("#settingsmodal").fadeIn();
-	}
+		this.settingsmodal.fadeIn();
+	};
 	Settings.prototype.close = function() {
-		$("#settingsmodal").fadeOut();
+		this.settingsmodal.fadeOut();
 		this.saveLocal();
-	}
+	};
 	Settings.prototype.setup = function() {
 		this.loadLocal();
 
 		this.settingsbutton = $("#settingsbutton");
 		this.settingsclose  = $("#settingsclose");
+		this.settingsmodal  = $("#settingsmodal");
 
 		this.settingsbutton.click(function(e) {
 			settings.open();
@@ -52,21 +54,28 @@
 			settings.close();
 		});
 
+		//Each checkbox should enable / disable the notification
 		$(".settingsnotif").each(function() {
+			//For reference
 			var jthis = $(this);
+			//Which setting are we?
 			var type = jthis.attr("setting");
+			//Initial value
 			this.checked = settings.shouldShowNotification(type);
 
+			//Add an event so we update window.settings when the check is clicked
 			jthis.click(function(e) {
 				window.settings.notifications[type] = jthis.is(":checked");
 			});
 		});
+
+		//Discard settings and logout
 		$("#settingsdiscard").click(function(e) {
 			settings.close();
 			webchat.logout();
 			webchat.setUser("", "", true);
 		});
-	}
+	};
 	window.settings = new Settings();
 	window.settings.setup();
 })(jQuery.noConflict());
